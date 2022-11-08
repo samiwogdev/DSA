f5519a9524085d6b9fefbf9b60c352855942a06b
Samuel Idowu Ogunbodede <samiwogdev@gmail.com>
2022-11-08 14:48:01 +0100

    updt_2

---
+++ src/TwoN.java
@@ -0,0 +1,12 @@
+public class TwoN {
+
+    public static void twoN(int n){
+        for (int i = 0; i < n; i++){
+            System.out.println(i);
+        }
+    }
+
+    public static void main(){
+        twoN(10);
+    }
+}
