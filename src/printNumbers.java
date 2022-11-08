2c8b0f7bf97eea1be6ade7cf2e8ec0177ea55fac
Samuel Idowu Ogunbodede <samiwogdev@gmail.com>
2022-11-08 15:26:37 +0100

    o(2n)

---
+++ src/printNumbers.java
@@ -0,0 +1,12 @@
+public class printNumbers {
+
+    public static void twoN(int n){ //0(2n)
+        for (int i = 0; i < n; i++){
+            System.out.println(i);
+        }
+
+        for (int j = 0; j < n; j++){
+            System.out.println(j);
+        }
+    }
+}
