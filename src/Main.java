54ed6ddd68e33753bbaa505913aa7794a715fac3
Samuel Idowu Ogunbodede <samiwogdev@gmail.com>
2022-11-15 08:11:34 +0100

    Java pointers

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,10 @@
 public class Main {
     public static void main(String[] args) {
-        printNumbers.twoN(10);
+//        printNumbers.twoN(10);
 //        printNumbers.nSqr(10);
 //        printNumbers.nCube(10);
+        int[] studentAge = {23, 18, 26, 19, 20};
+//        printNumbers.quadraticTimeComplexity(studentAge, 5);
+        printNumbers.fibonacci(4);
     }
 }
--- src/printNumbers.java
+++ src/printNumbers.java
@@ -36,7 +36,7 @@ public class printNumbers {
         System.out.println("Number printed in " + count + " times");
     }
 
-    //4. Drop the Non-dominance     n^3: O(m^2 + n)
+    //4. Drop the Non-dominance     n^3: O(m^2 + n) = o(n^2)
     public static void nCube(int n){
         int count = 0;
         for (int i = 0; i < n; i++){
@@ -53,5 +53,39 @@ public class printNumbers {
         System.out.println("Number printed in " + count + " times");
     }
 
+    //n^2: O(n^2)
+    static void quadraticTimeComplexity(int ages[], int size)
+    {
+        for (int i = 0; i < size; i++)
+        {
+            for (int j = 0; j < size; j++)
+            {
+                System.out.printf("%d = %d\n", ages[i], ages[j]);
+            }
+        }
     }
+
+    static int fibonacci(int num)
+    {
+        if (num <= 1){
+            return num;
+        }else {
+            return fibonacci(num - 2) + fibonacci(num - 1);
+        }
+    }
+
+    public static void fibonacciExample(int count){
+        int n1=0, n2=1, n3, i;
+
+        System.out.print(n1+" "+n2);//printing 0 and 1
+        for(i=2; i<count; ++i)//loop starts from 2 because 0 and 1 are already printed
+        {
+            n3=n1+n2;
+            System.out.print(" "+n3);
+            n1=n2;
+            n2=n3;
+        }
+        }
+
+}
 
